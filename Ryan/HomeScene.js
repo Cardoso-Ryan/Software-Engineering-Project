d7c8dd3e62a43557553d7521c76386e6f2beae02
Ryan CARDOSO <Ryan.Cardoso.001@student.uni.lu>
2024-04-21 23:32:07 +0200

    added Filo's game code to fuse it with the website

--- Ryan/HomeScene.js
+++ Ryan/HomeScene.js
@@ -4,24 +4,16 @@ class HomeScene extends Phaser.Scene {
     }
 
     preload(){
-        this.load.image("background", "assets/images/background.jpg");
-
         // Chargement de la vie des joueur en haut a droite
+        this.load.image("background", "assets/images/background.jpg");
         this.load.image("x", "assets/images/x.png");
         this.load.image("xf", "assets/images/xf.png");
         this.load.image("xx", "assets/images/xx.png");
         this.load.image("xxf", "assets/images/xxf.png");
         this.load.image("xxx", "assets/images/xxx.png");
         this.load.image("xxxf", "assets/images/xxxf.png");
-
-        //Chargement de la banane (a changer pour du bamboo)
         this.load.image("bamboo", "assets/images/bamboo.png");
-        this.load.image("bambooCut", "assets/images/bambooCut.png")
-        
-        this.load.spritesheet("smoke", "assets/smoke/smokes.png",{
-            frameWidth: 64,
-            frameHeight: 64
-        } )
+        this.load.image("bambooCut", "assets/images/bambooCut.png");   
     }
 
     create(){
--- Ryan/PlayScene.js
+++ Ryan/PlayScene.js
@@ -4,76 +4,105 @@ class PlayScene extends Phaser.Scene {
     }
 
     create(){
-        this.background = this.add.image(0,0,"background");
+        this.background = this.add.image(0, 0, "background").setOrigin(0, 0).setScale(1.6);
         this.background.setOrigin(0,0);
         this.background.setScale(1.6);
 
-        // Affichage de la vie du joueur
-        this.x = this.add.image(880, 35, "x");
-        this.x.setScale(1.2);
-        //this.x = this.add.image(890, 35, "xf");
-        //this.x.setScale(1.2);
-        this.x = this.add.image(920, 30, "xx");
-        this.x.setScale(1.4);
-        //this.x = this.add.image(920, 30, "xxf");
-        //this.x.setScale(1.4);
-        this.x = this.add.image(970, 25, "xxx");
-        this.x.setScale(1.6);
-        //this.x = this.add.image(970, 25, "xxxf");
-        //this.x.setScale(1.6);
+        // Customizable cursor
+        this.input.setDefaultCursor('url(assets/images/katanaMin.png), pointer');
 
-        // Affichage de la banane
-        this.bamboo = this.add.image(this.scale.width / 2, 100, "bamboo").setInteractive();
+        this.lives = ['x', 'xx', 'xxx']; // Array to manage lives left
+        this.livesSprites = {
+            x: this.add.image(880, 35, "x").setScale(1.2),
+            xx: this.add.image(920, 30, "xx").setScale(1.4),
+            xxx: this.add.image(970, 25, "xxx").setScale(1.6)
+        };
+
+        // Affichage du bamboo
+        this.bamboo = this.add.image(configScreen.width/2, configScreen.height/2, "bamboo");
+
         this.add.text(20, 20, "Playing game", {font: "25px KleeOne" , fill: "lime"});
-    
-        // Creation de l'animation de fumé en cliquant sur la bombe
-        this.anims.create({
-            key: "smoke",
-            frames: this.anims.generateFrameNumbers("smoke"),
-            frameRate: 20,
-            repeat: 0,
-            hideOnComplete: true
-        });
 
         //Interactivité de la banane et de la bombe
+        this.spawnNewBamboo();
         this.bamboo.setInteractive();
 
-        this.bamboo.on('pointerdown', () => this.destroyFruit(this.bamboo));
+        this.bamboo.on('gameobjectdown', this.destroyBamboo, this);
+
+        this.input.on('gameobjectover', (pointer, bamboo) => 
+        {
+            bamboo.setTexture("bambooCut");
+        });
+
     }
 
     update(){
-        this.updatebamboo();
+        this.updateBamboo();
     }
 
-    updatebamboo(){
+    updateBamboo(){
         // Rotation de la banane
         this.bamboo.angle += 2.5;
-        this.movebamboo(this.bamboo, 5); 
+        this.moveBamboo(this.bamboo, 5); 
     }
 
-    movebamboo(bamboo, speed) {
+    spawnNewBamboo() {
+        var randomX = Phaser.Math.Between(0, this.sys.game.config.width);
+        var bamboo = this.add.image(randomX, 0, "bamboo").setInteractive();
+        bamboo.cut = false; // Custom property to track if bamboo is cut
+        bamboo.on('pointerdown', () => {
+            bamboo.cut = true;
+            this.destroyBamboo(bamboo);
+        });
+
+        // Moving bamboo down the screen
+        this.tweens.add({
+            targets: bamboo,
+            y: this.sys.game.config.height + bamboo.height,
+            ease: 'Linear',
+            duration: 5000,
+            onComplete: () => {
+                this.resetBambooPos(bamboo);
+            }
+        });
+    }
+
+    moveBamboo(bamboo, speed) {
         bamboo.y += speed;
         if(bamboo.y > configScreen.height){
-            this.resetbambooPos(bamboo);
+            this.resetBambooPos(bamboo);
+            bamboo.setTexture("bamboo"); //reset a bamboo quand sort de la configScreen
         }
     }
 
-    resetbambooPos(bamboo){
+    resetBambooPos(bamboo){
+        if (!bamboo.cut) {
+            this.updateLives();
+        }
         bamboo.y = 0;
         var randomX = Phaser.Math.Between(0, configScreen.width);
         bamboo.x = randomX;
     }
 
-    destroyFruit(fruit) {
-        fruit.setTexture("bambooCut");
-        fruit.disableInteractive();
-        this.playSmokeAnimation(fruit);
+    destroyBamboo(pointer, gameObject){
+        gameObject.setTexture("bananaCut");
+        gameObject.disableInteractive();
+        this.playSmokeAnimation(gameObject);
     }
 
-    playSmokeAnimation(gameObject) {
-        gameObject.play("smoke"); // Play smoke animation
-        gameObject.on('animationcomplete', function () {
-            gameObject.destroy(); // Destroy object after animation completes
-        }, this);
+    updateLives() {
+        const life = this.lives.pop(); // Remove the last life from the array
+        if (life) {
+            this.livesSprites[life].setTexture(life + 'f'); // Update texture to show lost life
+        }
+        if (this.lives.length === 0) {
+            this.gameOver(); // Call game over method
+        }
     }
+
+    gameOver() {
+        console.log("Game Over"); // Replace with actual game over logic
+        // You can add a game over screen, restart option, etc.
+    }
+
 }
