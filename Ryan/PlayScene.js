1533fda7d10fbc563cc3e7c62ac9f53e2aca7f1a
Ryan CARDOSO <Ryan.Cardoso.001@student.uni.lu>
2024-05-21 20:42:23 +0200

    code cleaning

--- Ryan/PlayScene.js
+++ Ryan/PlayScene.js
@@ -17,15 +17,12 @@ class PlayScene extends Phaser.Scene {
     this.Pause();
     this.Resume();
     this.createBamboo();
-    //this.createBombs();
     this.createNameInput();
     this.createScoreboard();
     this.displayLevel();
     this.createObstacles();
     const Music = this.sound.add("Music", {volume: 0.3});
     Music.play();
-
-  //  const fx = this.bamboo.postFX.addGlow(0xffffff, 2, 0, false, 0.1, 32);
   }
 
   setupDifficulty() {
@@ -52,28 +49,14 @@ class PlayScene extends Phaser.Scene {
   update() {
     this.updateBamboo();
     this.updateBomb();
-    //this.updateObstacles();
-    //this.updateLevel();
   }
 
   Pause(){
     this.pause = this.add.image(900, 30, 'pauseBtn').setOrigin(0,0).setScale(0.2);
-   /* pauseBtn.on('onClick', function(){
-      sendToBack('PlayScene');
-      pause('PlayScene');
-      launch('pause');
-  });*/
 }
 
   Resume(){
   this.resume = this.add.image('resumeBtn').setScale(0.2);
-   /* resumeBtn.on('pointerdown', function(){
-    if(gameIsPaused === true){
-        gameIsPaused = false;
-        pauseBtn.setVisible(true);
-        pauseBtn.setActive(true);
-    }
-  });*/
 }
 
   createBackground() {
@@ -91,11 +74,10 @@ class PlayScene extends Phaser.Scene {
     this.bamboo.setInteractive();
     this.bamboo.isCut = false; 
     this.bamboo.on("pointerdown", this.killBamboo, this);
-    // Add pointerover event to cut the bamboo when mouse hovers over it
     this.bamboo.on("pointerover", () => {
-        if (!this.bamboo.isCut) { // Check if the bamboo has been cut
+        if (!this.bamboo.isCut) { 
             this.bamboo.setTexture("bambooCut");
-            this.bamboo.isCut = true; // Set the bamboo as cut
+            this.bamboo.isCut = true; 
             this.updateScore();
             let cutAudio = this.sound.add("cutAudio", {volume: 0.2});
             cutAudio.play();
@@ -103,29 +85,16 @@ class PlayScene extends Phaser.Scene {
     });
   }
 
- /* createBombs() {
-    this.bombs = this.physics.add.group({
-      key: 'bomb',
-      repeat: 5,
-      setXY: { x: Phaser.Math.Between(0, this.sys.game.config.width), y: 0, stepX: 70 }
-    });
-
-    this.bombs.children.iterate((bomb) => {
-      bomb.setInteractive();
-      bomb.on("pointerdown", this.killBomb, this);
-    });
-  }*/
-
   createObstacles() {
     this.bomb = this.add.image(this.sys.game.config.width / 2, this.sys.game.config.height / 2,"bomb");
     this.bomb.setInteractive();
     this.bomb.isCut = false; 
     this.bomb.on("pointerdown", this.killBomb, this);
-    // Add pointerover event to cut the bomb when mouse hovers over it
     this.bomb.on("pointerover", () => {
-        if (!this.bomb.isCut) { // Check if the bomb has been cut
+        if (!this.bomb.isCut) { 
             this.bomb.setTexture("smoke");
-            this.bomb.isCut = true; // Set the bomb as cut
+            this.bomb.isCut = true; 
+            this.decreaseScore(); // Decrease score by 10 points
             let explosion = this.sound.add("explosion", {volume: 0.1});
             explosion.play();
           }
@@ -136,19 +105,18 @@ class PlayScene extends Phaser.Scene {
     const style = { font: '32px Kanit', fill: 'white' };
     this.add.text(20, 50, "Name:", style);
   
-    // Create an input field using DOM element
     let element = this.add.dom(150, 100, 'input', {
       'type': 'text',
       'placeholder': 'Enter your name',
       'style': 'width: 200px; height: 24px; font-size: 22px; text-align: center; padding: 10px; border: none; border-radius: 4px; background-color: white; color: black;'
     });
   
-    element.node.focus();  // Automatically focus on the input field
+    element.node.focus();  
   
     element.addListener('input');
     element.on('input', (event) => {
       this.playerName = event.target.value;
-      this.updatePlayerNameDisplay(); // Update display if needed
+      this.updatePlayerNameDisplay(); 
     });
   }
 
@@ -171,13 +139,20 @@ class PlayScene extends Phaser.Scene {
   }
 
   updateScore() {
-    this.score += 1; // Increase score
-    this.scoreText.setText(this.score); // Update score text
+    this.score += 1; 
+    this.scoreText.setText(this.score); 
   
-    // Check if the score is a multiple of 20 to increase the level
     if (this.score % 5 === 0) {
       this.updateLevel();
     }
+  }
+
+  decreaseScore() {
+    this.score -= 10; 
+    if (this.score < 0) {
+      this.score = 0;
+    }
+    this.scoreText.setText(this.score);
   }
 
   displayLevel() {
@@ -187,19 +162,17 @@ class PlayScene extends Phaser.Scene {
   updateLevel() {
     this.level++;
     if (this.level >= 2) {
-      this.bambooScale = 0.8; // Scale up bamboo at level 2
+      this.bambooScale = 0.8; 
     }
     if (this.level >= 3) {
-      this.bambooSpeed += 1; // Increase speed at level 3
+      this.bambooSpeed += 1; 
     }
     if (this.level >= 4) {
-     // this.createObstacles(); // Introduce bombs at level 4
-     // this.updateObstacles();
     }
     if (this.level >= 5) {
       this.manageMultipleBamboos();
     }
-    this.bamboo.setScale(this.bambooScale); // Apply scale changes
+    this.bamboo.setScale(this.bambooScale); 
     this.levelText.setText('Level: ' + this.level);
   }
 
@@ -220,7 +193,6 @@ class PlayScene extends Phaser.Scene {
     }
   }
 
-
   updateBomb(){
     this.bomb.angle += 2;
     this.moveBomb(this.bomb, this.bombSpeed);
@@ -233,32 +205,9 @@ class PlayScene extends Phaser.Scene {
     }
   }
 
-  /*updateBombs() {
-    this.bombs.children.iterate((bomb) => {
-      bomb.y += this.bombSpeed;
-      if (bomb.y > this.sys.game.config.height) {
-        this.resetBombPos(bomb);
-      }
-    });
-  }
-
-  moveBomb(bomb, speed){
-    bomb.y += speed;
-    if (bomb.y > this.sys.game.config.height){
-      this.resetBombPos(bomb);
-    }
-  }*/
-
   updateObstacles() {
     this.bomb.angle += 2;
     this.moveObstacles(this.bomb, this.bombSpeed);
-    /*this.bombs.forEach(bomb => {
-      if (bomb.y > this.sys.game.config.height) {
-        // Reset bomb position to top with a new random x position
-        bomb.y = -50;
-        bomb.x = Phaser.Math.Between(100, this.sys.game.config.width - 100);
-      }
-    });*/
   }
 
   moveObstacles(bomb, speed) {
@@ -269,16 +218,14 @@ class PlayScene extends Phaser.Scene {
   }
 
   manageMultipleBamboos() {
-    // Check if there are fewer bamboos than the maximum allowed and add new ones
     if (this.bamboos.length < this.maxBamboos) {
       let newBamboo = this.createBamboo(Phaser.Math.Between(100, this.sys.game.config.width - 100), 0);
       this.bamboos.push(newBamboo);
     }
   
-    // Update position of each bamboo, reset if it goes off-screen
     this.bamboos.forEach(bamboo => {
       bamboo.y += this.bambooSpeed;
-      if (bamboo.y > this.sys.game.config.height + 100) { // Reset bamboo position if it moves off the bottom
+      if (bamboo.y > this.sys.game.config.height + 100) { 
         bamboo.y = 0;
         bamboo.x = Phaser.Math.Between(100, this.sys.game.config.width - 100);
         bamboo.isCut = false;
@@ -288,25 +235,23 @@ class PlayScene extends Phaser.Scene {
   }
 
   resetBambooPos(bamboo) {
-    bamboo.isCut = false; // Reset the cut status when bamboo is reset
+    bamboo.isCut = false;
     bamboo.setTexture("bamboo");
     bamboo.y = 0;
     bamboo.x = Phaser.Math.Between(0, this.sys.game.config.width);
   }
 
   resetBombPos(bomb){
-    bomb.isCut = false; // Reset the cut status when bomb is reset
+    bomb.isCut = false;
     bomb.setTexture("bomb");
     bomb.y = 0;
     bomb.x = Phaser.Math.Between(0, this.sys.game.config.width);
-    //bomb.setVisible(false);
   }
 
   killBamboo(pointer, gameObject) {
     if (!gameObject.isCut) {
         gameObject.setTexture("bambooCut");
         gameObject.disableInteractive();
-       // this.playSmokeAnimation(gameObject);
       }
   }
 
@@ -314,24 +259,9 @@ class PlayScene extends Phaser.Scene {
     if (!gameObject.isCut) {
         gameObject.setTexture("smoke");
         gameObject.disableInteractive();
-        bomb.setVisible(false);
-       // this.playSmokeAnimation(gameObject);
+        this.decreaseScore(); // Decrease score by 10 points
+        let explosion = this.sound.add("explosion", {volume: 0.1});
+        explosion.play();
       }
   }
-
- /* playSmokeAnimation(gameObject) {
-    this.anims.create({
-      key: "smoke",
-      url: "assets/images/smoke.png",
-      frames: this.anims.generateFrameNumbers("smoke"),
-      frameRate: 20,
-      repeat: 0,
-      hideOnComplete: true,
-    });
-
-    gameObject.play("smoke");
-    gameObject.on("animationcomplete", () => {
-      gameObject.kill();
-    });
-  }*/
 }
